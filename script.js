c7fd94eac9031ee625eeb3024c29cac740039e05
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-07-28 23:47:33 +0330

    Added streak system Tweaked multiplier decay and numbers Theme switch funtionality Toggle hide score

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 // DOM Elements:
-const letterEl = document.querySelector(".letter p")
-
+const letterEl = document.querySelector(".letter-and-multiplier p")
+const letterContainerEl = document.querySelector(".letter")
+const btnThemeSwitchEl = document.querySelector(".theme-switcher")
 const scoreMultiplierBarEl = document.querySelector(".score-multiplier")
 const leftIndicatorEl = document.querySelector(".indicator.left")
 const rightIndicatorEl = document.querySelector(".indicator.right")
 const scoreSpanEl = document.querySelector(".scores span")
 const lettersRemainingSpanEl = document.querySelector(".letters-remaining span")
 const nextLetterSpanEl = document.querySelector(".next-letter span")
-const appContainer = document.querySelector(".container")
+const currentStreakSpanEl = document.querySelector(".current-streak span")
+const maxStreakSpanEl = document.querySelector(".max-streak span")
+const accuracySpanEl = document.querySelector(".accuracy span")
+const appContainerEl = document.querySelector(".container")
 
 // DOM Function
-updateIndicatorAndLetterUI = (letter, mustPressLeftShift) => {
-  letterEl.innerText = letter
 
-  if (mustPressLeftShift) {
-    appContainer.dataset.whichShift = "left"
-  } else {
-    appContainer.dataset.whichShift = "right"
+const updateScoreVisibilityInUI = shown => {
+  scoreSpanEl.parentElement.parentElement.classList.add(
+    shown ? "shown" : "hidden"
+  )
+  scoreSpanEl.parentElement.parentElement.classList.remove(
+    shown ? "hidden" : "shown"
+  )
+  letterEl.style.borderRight = shown ? "1px solid" : "none"
+}
+
+const updateCurrentAndMaxStreakInUI = (streak, maxStreak) => {
+  maxStreakSpanEl.innerText = maxStreak
+  currentStreakSpanEl.innerText = streak
+
+  if (streak === maxStreak) {
+    maxStreakSpanEl.parentElement.classList.add("record-set")
+  }
+
+  if (streak === 0) {
+    currentStreakSpanEl.parentElement.style.display = "none"
+    maxStreakSpanEl.parentElement.style.display = "none"
+
+    maxStreakSpanEl.parentElement.classList.remove("record-set")
+    return
+  }
+
+  if (streak === 2) {
+    currentStreakSpanEl.parentElement.style.display = "block"
+    maxStreakSpanEl.parentElement.style.display = "block"
   }
 }
 
+const updateIndicatorAndLetterUI = (letter, mustPressLeftShift) => {
+  letterEl.innerText = letter
+
+  if (mustPressLeftShift) {
+    appContainerEl.dataset.whichShift = "left"
+  } else {
+    appContainerEl.dataset.whichShift = "right"
+  }
+}
+
+const updateAccuracyInUI = (typedLetters_correct, typedLetters) => {
+  const accuracyPercentage = (typedLetters_correct * 100) / typedLetters
+
+  accuracySpanEl.innerText =
+    typedLetters === 0 ? "-" : Math.ceil(accuracyPercentage * 100) / 100
+}
+
 const updateScoreInUI = amount => {
-  scoreSpanEl.innerText = amount
+  scoreSpanEl.innerText = Math.floor(amount)
 }
 
 const updateLettersRemainingInUI = lettersRemaining => {
@@ -45,6 +89,9 @@ const iterativeShuffleArray = array => {
 
   return newArray
 }
+
+const themeNames = ["light", "dark"]
+let currentThemeIndex = 0
 
 const englishAlphabet = [
   "a",
@@ -84,18 +131,38 @@ const lettersInfo = englishAlphabet.map(letter => ({
 let alphabetStack = iterativeShuffleArray(englishAlphabet)
 
 // config
+
+let scoreShown = true
+
+let streak = 0
+let streak_max = streak
+
 const scoreDecrementAmount_WrongShift = 4
 const scoreDecrementAmount_WrongLetter = 2
 const scoreDecrementAmount_TimeOut = 2
 const scoreIncrementAmount = 10
 
+let typedLetters = 0
+let typedLetters_correct = 0
+
 let score = 0
 let lettersRemaining = alphabetStack.length
 
 const scoreMultiplier = 200
+const maxEstimatedScoreMultiplierTime_MS = 7500
 let scoreMultiplier_current = scoreMultiplier
-let scoreMultiplier_decay = 0.1 * scoreMultiplier
-let scoreMultiplier_updateInterval_MS = 200
+let scoreMultiplier_updateInterval_MS = 100
+let scoreMultiplier_decay =
+  (scoreMultiplier_updateInterval_MS * 200) / maxEstimatedScoreMultiplierTime_MS
+
+const updateScoreMultiplierDecay = streak => {
+  scoreMultiplier_decay =
+    ((scoreMultiplier_updateInterval_MS * 200) /
+      maxEstimatedScoreMultiplierTime_MS) *
+    (1 + 0.3 * streak)
+
+  console.log(scoreMultiplier_decay)
+}
 
 const remove1LetterFromTheAlphabetStackAndUpdateUI = () => {
   if (alphabetStack.length === 1) {
@@ -156,32 +223,75 @@ document.addEventListener("keypress", e => {
       (mustPressLeftShift && currentShiftKeyState === SHIFT_KEY_STATES.LEFT) ||
       (!mustPressLeftShift && currentShiftKeyState === SHIFT_KEY_STATES.RIGHT)
     ) {
+      typedLetters_correct++
+      typedLetters++
+
       score += scoreIncrementAmount * scoreMultiplier_current
       scoreMultiplier_current = scoreMultiplier
+
+      streak++
+      if (streak > streak_max) streak_max = streak
+
+      updateCurrentAndMaxStreakInUI(streak, streak_max)
+      updateAccuracyInUI(typedLetters_correct, typedLetters)
       updateScoreInUI(score)
       remove1LetterFromTheAlphabetStackAndUpdateUI()
       return
     } else {
       // add wrong animations to ui
+      typedLetters++
       score -= scoreDecrementAmount_WrongShift * scoreMultiplier
+      streak = 0
+
+      updateCurrentAndMaxStreakInUI(streak, streak_max)
+      updateAccuracyInUI(typedLetters_correct, typedLetters)
       updateScoreInUI(score)
       return
     }
   }
   score -= scoreDecrementAmount_WrongLetter * scoreMultiplier
+  typedLetters++
+  streak = 0
+
+  updateCurrentAndMaxStreakInUI(streak, streak_max)
+  updateAccuracyInUI(typedLetters_correct, typedLetters)
   updateScoreInUI(score)
+})
+
+btnThemeSwitchEl.addEventListener("click", () => {
+  currentThemeIndex = (currentThemeIndex + 1) % themeNames.length
+  console.log(
+    `Theme ${currentThemeIndex}: ${themeNames[currentThemeIndex]} chosen`
+  )
+  appContainerEl.dataset.theme = themeNames[currentThemeIndex]
+})
+
+letterEl.addEventListener("click", () => {
+  scoreShown = !scoreShown
+  updateScoreVisibilityInUI(scoreShown)
 })
 
 // App starts here
 
+appContainerEl.dataset.theme = themeNames[currentThemeIndex]
+
 remove1LetterFromTheAlphabetStackAndUpdateUI()
+updateCurrentAndMaxStreakInUI(streak, streak_max)
+updateAccuracyInUI(typedLetters_correct, typedLetters)
 updateScoreInUI(score)
+updateScoreVisibilityInUI(scoreShown)
 
 setInterval(() => {
   scoreMultiplier_current = Math.max(
     scoreMultiplier_current - scoreMultiplier_decay,
     1
   )
+
+  if (scoreMultiplier_current === 1) {
+    streak = 0
+    updateCurrentAndMaxStreakInUI(streak, streak_max)
+  }
+
   scoreMultiplierBarEl.style.transform = `scaleX(
     ${scoreMultiplier_current / scoreMultiplier})`
   if (scoreMultiplier_current === 1) {
@@ -190,36 +300,40 @@ setInterval(() => {
       scoreMultiplier_updateInterval_MS
     updateScoreInUI(score)
   }
+
+  updateScoreMultiplierDecay(streak)
 }, scoreMultiplier_updateInterval_MS)
-
-/* TODO: Add a system that increases the multiplier when you type 2 write letters
-and gets back to normal if you break the streak
-
-if streak is 0 or broken current and max streak
- becomes display : none
-
- then if 2 answers given right then it shows it
-
- - when you streak like 5, then amount that fills gets decreased by a curve (like 10% off current value)
-
- */
-
-/* TODO: Toggle Hide Scores */
-
-/* TODO: Add Accuracy System */
 
 /* TODO: UI Elements for:
 
-    I) Timeout
-    II) Score: 100 (+100)
-    III) Gameover Screen 
+I) Timeout
+II) Score: 100 (+100)
+III) Gameover Screen 
 
 
 
 */
 
-/*TODO: Add Responsiveness */
+/*TODO: Style the theme button */
 
+/*TODO: Add Responsiveness */
 /* TODO: Add documentation */
 
 /*TODO: Add sounds for right and wrong */
+
+/* TODO: Add Accuracy System --DONE */
+
+/* TODO: Add a system that increases the multiplier when you type 2 write letters
+and gets back to normal if you break the streak
+
+if streak is 0 or broken current and max streak
+becomes display : none
+
+then if 2 answers given right then it shows it
+
+- when you streak like 5, then amount that fills gets decreased by a curve (like 10% off current value)
+
+--DONE
+*/
+
+/* TODO: Toggle Hide Scores --DONE */
