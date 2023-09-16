26d3fccb427b2fff9afa2121f8f25eaa331f0760
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-09-16 21:30:19 +0330

    refactored english letter data

---
+++ data/en.js
@@ -0,0 +1,132 @@
+const enLetters = [
+  {
+    letter: "a",
+    capitalizedLetter: "A",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "b",
+    capitalizedLetter: "B",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "c",
+    capitalizedLetter: "C",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "d",
+    capitalizedLetter: "D",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "e",
+    capitalizedLetter: "E",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "f",
+    capitalizedLetter: "F",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "g",
+    capitalizedLetter: "G",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "h",
+    capitalizedLetter: "H",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "i",
+    capitalizedLetter: "I",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "j",
+    capitalizedLetter: "J",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "k",
+    capitalizedLetter: "K",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "l",
+    capitalizedLetter: "L",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "m",
+    capitalizedLetter: "M",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "n",
+    capitalizedLetter: "N",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "o",
+    capitalizedLetter: "O",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "p",
+    capitalizedLetter: "P",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "q",
+    capitalizedLetter: "Q",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "r",
+    capitalizedLetter: "R",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "s",
+    capitalizedLetter: "S",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "t",
+    capitalizedLetter: "T",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "u",
+    capitalizedLetter: "U",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "v",
+    capitalizedLetter: "V",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "w",
+    capitalizedLetter: "W",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "x",
+    capitalizedLetter: "X",
+    mustPressLeftShift: false
+  },
+  {
+    letter: "y",
+    capitalizedLetter: "Y",
+    mustPressLeftShift: true
+  },
+  {
+    letter: "z",
+    capitalizedLetter: "Z",
+    mustPressLeftShift: false
+  }
+]
